4a29b3909e05d24a7e362bdbbac3707e6ce8f1aa
Narasimha997 <gkumar301099@gmail.com>
2023-01-11 12:42:49 +0530

    React

---
+++ react-1/src/App.js
@@ -0,0 +1,15 @@
+// import { Children } from "react";
+
+function App(){
+    return <div>
+        <h1>Gm</h1>
+        <h1>Gm</h1>
+        <h1>Gm</h1>
+        <h1>Gm</h1>
+        <h1>Gm</h1>
+        <h1>Gm</h1>
+        <h1>Gm</h1>
+        <h1>Gm</h1>
+    </div>
+}
+export default App
